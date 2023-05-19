71fc489610f84d76d24f38f961cdc83dd86f2e39
Mohammad Fikri Nur Syahbani <72445001+mohammadfikrinursyahbani@users.noreply.github.com>
2023-05-19 14:39:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,20 +28,19 @@ def app():
     st.write('Enter the following details to predict whether you have diabetes or not.')
 
     # Create input fields for the user to enter the details
-    pregnancies = st.slider('Pregnancies', 0, 17, 0)
-    glucose = st.slider('Glucose', 0, 199, 70)
-    blood_pressure = st.slider('Blood Pressure', 0, 122, 70)
-    skin_thickness = st.slider('Skin Thickness', 0, 99, 20)
-    insulin = st.slider('Insulin', 0, 846, 79)
-    bmi = st.slider('BMI', 0.0, 67.1, 20.0)
-    diabetes_pedigree_function = st.slider('Diabetes Pedigree Function', 0.078, 2.42, 0.3725)
-    age = st.slider('Age', 21, 81, 33)
+    pregnancies = st.number_input('Pregnancies', min_value=0, max_value=17, step=1, value=0)
+    glucose = st.number_input('Glucose', min_value=0, max_value=199, value=70)
+    blood_pressure = st.number_input('Blood Pressure', min_value=0, max_value=122, value=70)
+    skin_thickness = st.number_input('Skin Thickness', min_value=0, max_value=99, value=20)
+    insulin = st.number_input('Insulin', min_value=0, max_value=846, value=79)
+    bmi = st.number_input('BMI', min_value=0.0, max_value=67.1, value=20.0)
+    diabetes_pedigree_function = st.number_input('Diabetes Pedigree Function', min_value=0.078, max_value=2.42, value=0.3725)
+    age = st.number_input('Age', min_value=21, max_value=81, value=33)
 
     # Check if any input field is empty
     if glucose == 0 or blood_pressure == 0 or skin_thickness == 0 or insulin == 0 or bmi == 0.0 or diabetes_pedigree_function == 0.0 or age == 0:
         st.write('Not allowed to enter a value of 0 except for pregnancies.')
-    else:
-        # Call the predict function and display the result
+    else:# Check if any input field is empty
         if st.button('Predict'):
             result = predict_diabetes(pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age)
             if result == 0:
